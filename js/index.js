9f66c885e8e318a2a84141b3363856c7980328c4
gi-ogawa <ogawa_h@gp1000.jp>
2025-11-12 14:47:57 +0900

    jsのモーダルコード練習

--- js/index.js
+++ js/index.js
@@ -7,3 +7,26 @@ btn.addEventListener("click",() =>
 {
   menu.classList.toggle("show");
 });
+
+const modal = document.querySelector('.js-modal'),
+      open = document.querySelector('.js-modal-open'),
+      close = document.querySelector('.js-modal-close');
+
+function modalOpen(){
+  modal.classList.add('is-active');
+}
+open.addEventListener('click',modalOpen);
+
+
+function modalClose(){
+  modal.classList.remove('is-active');
+  
+}
+close.addEventListener('click',modalClose);
+
+function modalOut(e){
+  if(e.target == modal){
+    modal.classList.remove('is-active');
+  }
+}
+addEventListener('click',modalOut);
