22171984b779a74dc04eefa4c22dfc4b5bad64c4
gi-ogawa <ogawa_h@gp1000.jp>
2025-11-18 14:53:07 +0900

    jsのスライドショーのコード練習

--- js/index.js
+++ js/index.js
@@ -30,3 +30,71 @@ function modalOut(e){
   }
 }
 addEventListener('click',modalOut);
+
+
+const images = document.querySelectorAll('img');
+const navContainer = document.querySelector('.nav-container');
+const totalImages = images.length
+let imageIndex = 0;
+let interval;
+const button = document.createElement('button');
+button.classList.add('nav-btn');
+navContainer.appendChild(button);
+for (let i = 0; i < totalImages; i++){
+  const button = document.createElement('button');
+  button.classList.add('nav-btn');
+  navContainer.appendChild(button);
+}
+
+const buttons = document.querySelectorAll('.nav-btn');
+buttons.forEach(() => {
+
+});
+
+buttons.forEach((button,index) => {
+  button.addEventListener('click',() => {
+    imageIndex = index;
+    updateSlider();
+  });
+});
+
+function updateSlider() {
+  images.forEach(image => {
+    image.classList.remove('image-active');
+  });
+
+  buttons.forEach(button => {
+    button.classList.remove('btn-active');
+  });
+
+  images[imageIndex].classList.add('image-active');
+  buttons[imageIndex].classList.add('btn-active');
+}
+
+function nextImage(){
+  imageIndex++;
+  if (imageIndex > totalImages - 1){
+    imageIndex = 0;
+  }
+  updateSlider();
+}
+
+function autoPlay(){
+  interval = setInterval(nextImage, 3000);
+}
+
+function resetInterval(){
+  clearInterval(interval);
+  autoPlay();
+}
+
+buttons.forEach((button,index) => {
+  button.addEventListener('click',() => {
+    imageIndex = index;
+    updateSlider();
+    resetInterval();
+  });
+});
+
+updateSlider();
+autoPlay();
